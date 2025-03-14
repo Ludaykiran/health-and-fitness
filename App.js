ade80e8956cec3a9bada45ee8fe5da5f50349d66
Udaykiran <96521240+Ludaykiran@users.noreply.github.com>
2025-03-14 15:34:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
-// // import React from "react";
-// // import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from "react-router-dom";
-// // import Login from "./Login";
-// // import Navbar from "./Navbar";
-// // import Pricing from "./Pricing";
-// // import Dashboard from "./Dashboard";
-// // import Signup from "./SignUp";
-// // import Shop from "./Shop";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from "react-router-dom";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import Pricing from "./Pricing";
+import Dashboard from "./Dashboard";
+import Signup from "./SignUp";
+import Shop from "./Shop";
 
-// // const App = () => {
-// //   return (
-// //     <Router>
-// //       <Main />
-// //     </Router>
-// //   );
-// // };
+const App = () => {
+  return (
+    <Router>
+      <Main />
+    </Router>
+  );
+};
 
-// // const Main = () => {
-// //   const location = useLocation();
-// //   const hideNavbarRoutes = ["/login", "/signup"]; // Hide navbar on these pages
+const Main = () => {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/login", "/signup"]; // Hide navbar on these pages
 
-// //   return (
-// //     <>
-// //       {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
-// //       <Routes>
-// //         <Route path="/" element={<Navigate to="/login" />} />
-// //         <Route path="/login" element={<Login />} />
-// //         <Route path="/pricing" element={<Pricing />} />
-// //         <Route path="/dashboard" element={<Dashboard />} />
-// //         <Route path="/signup" element={<Signup />} />
-// //         <Route path="/shop" element={<Shop />}/>
-// //       </Routes>
-// //     </>
-// //   );
-// // };
+  return (
+    <>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/shop" element={<Shop />}/>
+      </Routes>
+    </>
+  );
+};
